4db36bb4dd67f369b518dc6bb5d916bd0ac40cc2
devmoaid1 <moaidmohamed1312@gmail.com>
2022-11-20 02:09:27 +0200

    click event listener

--- app.js
+++ app.js
@@ -7,10 +7,21 @@ const fragment = document.createDocumentFragment()
 const navBar=document.getElementById('navbar__list')
 
 for(let i =1; i<=sections.length;i++){
-    const navItem=document.createElement('a')
-    navItem.setAttribute('href',"#section"+i)
-    navItem.textContent="Section"+i 
+    const navItem=document.createElement('div')
+    navItem.setAttribute('class',"nav_item")
+    navItem.textContent="Section"+i
+    navItem.addEventListener('click',function(event){
+        console.log("hey")
+    }) 
     fragment.appendChild(navItem)
 }
 
 navBar.appendChild(fragment)
+
+
+const navItem= document.getElementsByClassName('nav_item') 
+
+
+
+
+
