24c4dacd940a35116423543ce1a199f08caac93e
devmoaid1 <moaidmohamed1312@gmail.com>
2022-11-20 22:23:31 +0200

    refactoring

--- js/app.js
+++ js/app.js
@@ -5,38 +5,74 @@ const sections=document.querySelectorAll('section') // all sections
 const fragment = document.createDocumentFragment()
 const navBar=document.getElementById('navbar__list') // nav bar container
 
-
-const generateNavItems=function(sectionsList){
+// generate nav items based on sections
+const generateNavItems=(sectionsList)=>{
    
-    sectionsList.forEach(function(item,index){
+    sectionsList.forEach((item,index)=>{
         const navItem=document.createElement('a') // create a div containe for nav item
         navItem.setAttribute('class',"nav_item") // set a class for css styling
-        navItem.setAttribute('href','#'+item.id)
-        console.log(item.id)
+        navItem.setAttribute('href','#'+item.id) // for navigation to section 
+        
         navItem.textContent=item.firstElementChild.firstElementChild.textContent // content refers for section heading 
             
         fragment.appendChild(navItem) // append new navItem to fragment
     })
 } 
 
-// const handleNavItemClick=function(event){
-   
-//    event.preventDefault()
-//    const section= document.getElementById(event.target.textContent.split(" ").join("")) // get corresponding section element for target nav item
-//    console.log(event.target.textContent)
-//    window.scrollTo()
-// }
 
+// check if section is in viewPort
+const isInViewPort=(element)=>{
+   
+   const rect=element.getBoundingClientRect()
+
+   
+   return(
+    rect.top >= 0 &&
+	rect.left >= 0 &&
+	rect.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+	rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+   )
+
+  
+    
+}
+
+
+// handle set active class for scrolling event
+const updateSectionClassActive=()=>{
+  for(const section of sections){
+   if(isInViewPort(section)){
+      if(section.classList.length==0){
+        section.classList.add('class')
+        section.setAttribute('class',"your-active-class")
+      }   
+   }else{
+    section.classList.remove('class')
+   }
+  }
+}
+
+
+// handle click event on navItem
+const handleNavItemClick=event=>{
+    const section = document.querySelector(event.target.attributes['href'].value)
+    section.setAttribute('class','your-active-class')
+
+}
 
 
 
 generateNavItems(sections)
-
 navBar.appendChild(fragment)  
-// navBar.addEventListener('click',handleNavItemClick) 
 
 
-const navItem= document.getElementsByClassName('nav_item') 
+navBar.addEventListener('click',handleNavItemClick)
+
+
+document.addEventListener('scroll',updateSectionClassActive)
+
+
+
 
 
 
